da77aa755132afa3f29955cdc6f5348a316d56d9
Dmitry Kurilo <dima@kurilo.su>
2015-08-07 02:24:51 +0300

    updated

--- setviewport.js
+++ setviewport.js
@@ -5,45 +5,68 @@
  *
  * Add to a <head>:
  * <meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+ *
+ * And before </body>
+ *
  * <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
  * <script>window.jQuery || document.write('<script src="jquery-1.11.1.min.js"><\/script>')</script>
  * <script src="setviewport.js"></script>
  */
 // set viewport for mobile devices
+'use strict';
 (function() {
-  var widthToSet = 1024, // width to achieve
+  var widthToSet = 640, // width to achieve,
+      widthToSetHoriz = 1282,
       scalable = false,  // make it scalable
       minScale = 1,      // set minimum scale
       maxScale = 1;      // set maximum scale
   var ios7safariregex = new RegExp(/.*iP[ha].*;.*CPU.*OS \d\_\d.*Safari.*/i);
   var ios7 = ios7safariregex.test(navigator.userAgent);
-  var ie9 = navigator.userAgent.toLowerCase().indexOf( 'iemobile/9.0' ) != -1;
-  var ie = navigator.userAgent.toLowerCase().indexOf( 'iemobile' ) != -1;
+  var ie9 = navigator.userAgent.toLowerCase().indexOf( 'iemobile/9.0' ) !== -1;
+  var ie = navigator.userAgent.toLowerCase().indexOf( 'iemobile' ) !== -1;
+  var iwidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var iheight = window.innerHeight || document.documentElement.clientHeight|| document.body.clientHeight;
+  var horiz = false;
+  if (iheight < iwidth) {
+    horiz = true;
+  }
+  if (horiz) {
+    iwidth = iheight;
+    widthToSet = widthToSetHoriz;
+  } else if (iwidth > 640) {
+    widthToSet = 1124;
+  }
+  if ( iwidth > 1024 && navigator.userAgent.toLowerCase().indexOf( 'ipad' ) !== -1 ) {
+    $('head').append('<meta name="ismobile" content="">');
+  }
+  if (iwidth > 1024) {
+    return;
+  }
+  $('head').append('<meta name="ismobile" content="">');
+  $('head meta[name="viewport"]').remove();
   if( ie9 ) {
-    document.write( '<meta name="viewport" content="width=device-width, user-scalable='+(scalable?'yes':'no')+'">' );
-    document.write( '<link rel="stylesheet" type="text/css" href="/mobile/css/ie9.css" />' );
+    $('head').append( '<meta name="viewport" content="width=device-width, user-scalable='+(scalable?'yes':'no')+'">' );
+    $('head').append( '<link rel="stylesheet" type="text/css" href="/assets/css/ie.css" />' ); // if if you need additional styles for IE
   } else if( ie  ) {
-    document.write( '<link rel="stylesheet" type="text/css" href="/mobile/css/ie.css" />' );
-  } else if ( navigator.userAgent.toLowerCase().indexOf( 'android 2.3.6' ) != -1  ) {
-    document.write( '<meta name="viewport" content="width='+($(window).width()>0?$(window).width()-80:widthToSet)+', user-scalable='+(scalable?'yes':'no')+'">' );
+    $('head').append( '<link rel="stylesheet" type="text/css" href="/assets/css/ie.css" />' ); // if if you need additional styles for IE
+  } else if ( navigator.userAgent.toLowerCase().indexOf( 'android 2.3.6' ) !== -1  ) {
+    $('head').append( '<meta name="viewport" content="width='+($(window).width()>0?$(window).width()-80:widthToSet)+', user-scalable='+(scalable?'yes':'no')+'">' );
   } else if (ios7) {
-    document.write( '<meta name="viewport" content="width='+widthToSet+', user-scalable='+(scalable?'yes':'no')+'">' );
-  } else if ( navigator.userAgent.toLowerCase().indexOf( 'iphone' ) != -1 ) {
-    document.write( '<meta name="viewport" content="width='+widthToSet+', user-scalable='+(scalable?'yes':'no')+'">' );
-  } else if ( navigator.userAgent.toLowerCase().indexOf( 'ipad' ) != -1 ) {
-    document.write( '<meta name="viewport" content="width='+widthToSet+', user-scalable='+(scalable?'yes':'no')+'">' );
+    $('head').append( '<meta name="viewport" content="width='+widthToSet+', user-scalable='+(scalable?'yes':'no')+'">' );
+  } else if ( navigator.userAgent.toLowerCase().indexOf( 'iphone' ) !==-1 ) {
+    $('head').append( '<meta name="viewport" content="width='+widthToSet+', user-scalable='+(scalable?'yes':'no')+'">' );
+  } else if ( navigator.userAgent.toLowerCase().indexOf( 'ipad' ) !== -1 ) {
+    $('head').append( '<meta name="viewport" content="width='+widthToSet+', user-scalable='+(scalable?'yes':'no')+'">' );
   } else {
-    document.write( '<meta name="viewport" content="width=360,initial-scale=1.0,target-densityDPI=360dpi">' );
     $(function(){
       $('html,body').animate({scrollTop: 0}, 1);
       setTimeout(function (){
-        var w1 = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
         $('meta[name="viewport"]').remove();
-        var x = 360/w1;
+        var x = 360/iwidth;
         var out = Math.ceil(widthToSet*x);
-        $('<meta name="viewport" id="the_vp_special" content="width=360,target-densityDPI='+out+'dpi,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable='+(scalable?'yes':'no')+'">').appendTo('head');//insert the new meta tag on the head  
-        var dimension = window.innerHeight < window.innerWidth ? window.innerHeight : window.innerWidth
-        if (window.devicePixelRatio !== undefined && dimension != widthToSet) {
+        $('<meta name="viewport" id="the_vp_special" content="width=360,target-densityDPI='+out+'dpi,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable='+(scalable?'yes':'no')+'">').appendTo('head');//insert the new meta tag on the head
+        var dimension = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        if (window.devicePixelRatio !== undefined && dimension !== widthToSet) {
           var viewPortScale = dimension / widthToSet;
           $('#the_vp_special').attr('content', 'user-scalable='+(scalable?'yes':'no')+', initial-scale='+viewPortScale+',maximum-scale='+(viewPortScale*maxScale)+',minimum-scale='+(viewPortScale*minScale)+', width=device-width');
         }
